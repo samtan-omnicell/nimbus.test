5fd50b37be2b8b3c453942d5a0ee79a61562453e
Peset Sam Tan <pesettan@gmail.com>
2021-03-07 09:33:34 -0800

    initial commit

---
+++ nimbus.test/Controllers/PingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace nimbus.test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        private readonly ILogger<PingController> _logger;
+
+        public PingController(ILogger<PingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("Returning Pong to the Ping");
+            return "pong";
+        }
+    }
+}
